2d2246b282d65ec0b7d471eef037b9969081e55e
eokpere <evelynokpere@outlook.com>
2023-01-25 20:10:11 -0500

    first commit

---
+++ remote-ec2.tf
@@ -0,0 +1,34 @@
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "FusionIT"
+    workspaces = {
+      name = "gh-action-demo-eva"
+    }
+  }
+}
+
+/*
+# Terraform >= 0.12
+resource "aws_instance" "foo" {
+  # ...
+  subnet_id = data.terraform_remote_state.vpc.outputs.subnet_id
+}
+
+# Terraform <= 0.11
+resource "aws_instance" "foo" {
+  # ...
+  subnet_id = "${data.terraform_remote_state.vpc.subnet_id}"
+}
+*/
+
+resource "aws_instance" "my-ec2" {
+  ami = data.aws_ami.amzlinux2.id
+  instance_type = "t2.micro" 
+  key_name = "mykeypair"
+  vpc_security_group_ids = data.terraform_remote_state.aws_security_group.vpc-ssh.id
+  count = 1
+  tags = {
+    "Name" = "Staging-${count.index}"
+  }
